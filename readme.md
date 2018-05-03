1e6da503447329ea5f7e884786675f5599319545
Nimesh Jain <pancholinimesh25@gmail.com>
2018-05-03 21:20:48 +0530

    add test for POST /todos route

---
+++ readme.md
@@ -0,0 +1,8 @@
+### installing modules
+`npm i`
+
+### for testing
+
+note: for now if you run testing code, it currently wipes out local database
+
+`npm run test-watch`
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
-//mongoose config
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-//ES6 new object literal notation
 module.exports = {mongoose};
