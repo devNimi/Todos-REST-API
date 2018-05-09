76400c362aeff7d869bb2ec2af9797210ee5b3c4
Nimesh Jain <pancholinimesh25@gmail.com>
2018-05-09 19:15:12 +0530

    Add tests for POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -45,9 +45,8 @@ UserSchema.methods.generateAuthToken = function () {
   var access = 'auth';
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens = user.tokens.concat([{access, token}]);
+  user.tokens.push({access, token});
 
-  console.log(user);
   return user.save().then(() => {
     return token;
   });
